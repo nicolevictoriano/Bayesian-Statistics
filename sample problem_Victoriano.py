bcd7ba33b8bdfb2731c2f15bac056d3730bcba1a
nicolevictoriano <victorianonicole1@gmail.com>
2024-05-18 09:51:59 +0800

    Add files via upload

---
+++ sample problem_Victoriano.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 17:13:42 2024
+
+@author: nicole victoriano
+"""
+
+def bayes_theorem(p_a, p_b_given_a, p_b_given_not_a):
+    not_a = 1 - p_a
+    p_b = p_b_given_a * p_a + p_b_given_not_a * not_a
+    p_a_given_b = (p_b_given_a * p_a) / p_b
+    return p_a_given_b
+
+p_a=0.0002
+p_b_given_a=0.85
+p_b_given_not_a=0.05
+result=bayes_theorem(p_a, p_b_given_a, p_b_given_not_a)
+print('P(A|B) = %.3f%%' % (result * 100))
